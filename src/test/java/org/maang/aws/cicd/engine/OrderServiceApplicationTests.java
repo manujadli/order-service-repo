b4a203b094760915262dc16cf6d17bb7a2e5b42d
manujadli <manujadli@gmail.com>
2022-11-25 16:36:12 +0530

    First Commit for AWS Code pipe line

---
+++ src/test/java/org/maang/aws/cicd/engine/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maang.aws.cicd.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
